b04c1e2539634f2317e47294eccb92c9352d5517
asifiqbal-hubgit <asifiqbal7711@gmail.com>
2025-07-06 21:27:07 +0500

    update

--- List.py
+++ List.py
@@ -1,2 +1,24 @@
-thislist_ = [1,2,3,4]
-print(thislist_)
+#List creation
+mylist_flowers  = ["Rose", "Tulip", "Sunflower", "Daisy", "Marigold", "Jasmine", "lotus"]
+print(mylist_flowers )
+
+#Mutable: The elements of the list can be modified. We can add or remove items to the list after it has been created.
+#Ordered: The items in the lists are ordered. Each item has a unique index value. The new items will be added to the end of the list.
+#Heterogenous: The list can contain different kinds of elements i.e; they can contain elements of string, integer, boolean, or any type.
+#Duplicates: The list can contain duplicates i.e., lists can have two items with the same values.
+
+#Allow Duplicates, List Length, 
+car_list = ["Toyota", "Honda", "Chevrolet", "BMW", "Ford", "Handa", "BMW"]
+print(len(car_list))
+
+#The list() Constructor
+furniture_list = (("chairs", "tables", "beds", "sofas", "dressers"))
+print(furniture_list)
+
+#Type of list
+furniture_list = ["chairs", "tables", "beds", "sofas", "dressers"]
+print(type(furniture_list))
+
+#List Items - Data Types
+list = ["abc", 123, "Ture", 50,]
+print(list)
