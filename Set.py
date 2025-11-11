6cea938c1faa6384975b245c9d701b952576b3ba
asifiqbal-hubgit <asifiqbal7711@gmail.com>
2025-11-11 16:59:25 +0500

    Create Set.py
    My set

---
+++ Set.py
@@ -0,0 +1,7 @@
+#A set is a collection of unique, unordered items.
+#It doesn’t allow duplicates.
+#The elements are unordered (so no indexing).
+#Sets are useful for removing duplicates or doing mathematical operations like union, intersection, etc.
+#Sets are written with curly brackets.
+my_set = {10, 20, 30, 40, 50}
+print(my_set)
