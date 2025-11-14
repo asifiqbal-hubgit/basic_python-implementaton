d13cca652125e00fed2ef3f6c958cf725604b31b
asifiqbal-hubgit <asifiqbal7711@gmail.com>
2025-11-14 15:17:13 +0500

    Update Set.py
    Sets Examples

--- Set.py
+++ Set.py
@@ -3,5 +3,31 @@
 #The elements are unordered (so no indexing).
 #Sets are useful for removing duplicates or doing mathematical operations like union, intersection, etc.
 #Sets are written with curly brackets.
-my_set = {10, 20, 30, 40, 50}
+my_set = {1, 2, 3, 4, 5}
 print(my_set)
+
+#Key Features: 
+#No duplicates 
+my_set = {10, 20, 20, 30, 40, 50}
+print(my_set)
+#No indexing
+my_set = {10, 20, 20, 30, 40, 50}
+print(my_set[0])
+#Length of a Set
+my_set = {10, 20, 20, 30, 40, 50}
+print(len(my_set))
+#type()
+my_set = {10, 20, 20, 30, 40, 50}
+print(type(my_set))
+#String, int and boolean data types:
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+#A set with strings, integers and boolean values:
+thisset = {"Asif", 32, "true", 50, "Male"}
+print(thisset)
+#
